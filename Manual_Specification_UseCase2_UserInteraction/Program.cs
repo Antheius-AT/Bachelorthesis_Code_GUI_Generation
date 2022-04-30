e4be01a620ab2c99672ba6939524a79e30b55f14
Antheius-AT <53980774+Antheius-AT@users.noreply.github.com>
2022-04-30 15:18:11 +0200

    Added projects for reflection and manual approaches per use case

---
+++ Manual_Specification_UseCase2_UserInteraction/Program.cs
@@ -0,0 +1,11 @@
+using Manual_Specification_UseCase2_UserInteraction;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();

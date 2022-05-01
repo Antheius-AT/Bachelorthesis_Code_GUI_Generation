be73352165b27af31b99930cdb11a8e2f9c2eb7e
Antheius-AT <53980774+Antheius-AT@users.noreply.github.com>
2022-05-01 21:18:04 +0200

    Added simple language specification file to wrap my head around the idea more easily and experimented with translation model -> abstract ui as well as abstract ui -> concrete UI

---
+++ GUI_Generator_UseCase1_Display/AUIEmitter.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace GUI_Generator_UseCase1_Display
+{
+    public class AUIEmitter
+    {
+        private List<Type> primitiveTypes;
+
+        public AUIEmitter()
+        {
+            this.primitiveTypes = new List<Type>()
+            {
+                typeof(string),
+                typeof(int),
+                typeof(float),
+                typeof(double),
+                typeof(bool),
+            };
+        }
+
+        public string Emit(object @object, string specification = "")
+        {
+            var properties = @object.GetType().GetProperties();
+
+            foreach (var item in properties)
+            {
+                // If item is primitive type directly translate it
+                // If item is complex type build container and start process anew with property being the new object.
+
+                if (primitiveTypes.Contains(item.PropertyType))
+                {
+                    specification += $"<element type=\"{item.PropertyType}\" value=\"{item.GetValue(@object)}\" label=\"{item.Name}\"/>\n";
+                }
+                else
+                {
+                    specification += "<container>";
+                    var containerContent = this.Emit(item, specification);
+                    specification += $"{containerContent}</container>";
+                }
+            }
+
+            return specification;
+        }
+    }
+}
---
+++ GUI_Generator_UseCase1_Display/CUIEmitter.cs
@@ -0,0 +1,122 @@
+﻿using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Models.UseCases.DisplayOnly.UseCase1;
+using Radzen;
+using Radzen.Blazor;
+
+namespace GUI_Generator_UseCase1_Display
+{
+    public class CUIEmitter
+    {
+        private Dictionary<Type, RadzenComponent> componentMap;
+        private SensorData data;
+
+        public CUIEmitter(SensorData data)
+        {
+            componentMap = new Dictionary<Type, RadzenComponent>()
+            {
+                {typeof(bool), new RadzenSwitch() },
+                {typeof(double), new RadzenNumeric<double>() },
+                {typeof(int), new RadzenNumeric<int>() },
+                {typeof(string), new RadzenTextBox() },
+            };
+
+            this.data = data;
+        }
+
+        public RenderFragment Emit(string specification)
+        {
+            var specificationToArray = specification.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            var components = new List<string>();
+
+            foreach (var item in specificationToArray)
+            {
+                if (item.StartsWith("<element"))
+                {
+                    var entry = item.Split(new string[] { "<element", "/>" }, StringSplitOptions.RemoveEmptyEntries).First();
+                    var component = MapEntry(entry);
+
+                    components.Add(component);
+                }
+                else if (item.StartsWith("<container"))
+                {
+                    throw new NotImplementedException();
+                }
+                else
+                {
+                    throw new ArgumentException(nameof(specification), "Specification is not valid");
+                }
+            }
+
+
+            return new RenderFragment(builder =>
+            {
+                for (int i = 0; i < components.Count; i++)
+                {
+                    var markup = components[i].Replace(',', '.');
+                    Console.WriteLine(markup);
+                    builder.AddMarkupContent(i, markup);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Maps an abstract definition to a concrete element and returns a html string that can be rendered by a render fragment.
+        /// </summary>
+        /// <param name="entry"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="Exception"></exception>
+        private string MapEntry(string entry)
+        {
+            // At this point entry contains the attributes of the element tag.
+            var elementAttributes = entry.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string? typeName = null;
+            string? value = null;
+            string? label = null;
+
+            foreach (var item in elementAttributes)
+            {
+                if (item.StartsWith("type"))
+                {
+                    typeName = item.Split("\"", StringSplitOptions.RemoveEmptyEntries).Last();
+                }
+                else if (item.StartsWith("value"))
+                {
+                    value = item.Split("\"", StringSplitOptions.RemoveEmptyEntries).Last();
+                }
+                else if (item.StartsWith("label"))
+                {
+                    label = item.Split("\"", StringSplitOptions.RemoveEmptyEntries).Last();
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(typeName))
+            {
+                throw new ArgumentException(nameof(entry), "Type information was missing in abstract UI specification for entry");
+            }
+
+            var type = Type.GetType(typeName) ?? throw new ArgumentException(nameof(entry), "Type information was missing in abstract UI specification for entry"); ;
+
+            var control = this.componentMap[type];
+
+            if (type == typeof(string) && control is FormComponent<string>)
+            {
+                return $"<input type=text disabled/>";
+            }
+            else if (type == typeof(double) && control is FormComponent<double>)
+            {
+                return $"<input type=number disabled @bind-Value={data.CurrentTemperature}/>";
+            }
+            else if (type == typeof(bool) && control is FormComponent<bool>)
+            {
+                return $"<input type=checkbox disabled @bind-Value={data.IsPoweredOn}>";
+            }
+
+            throw new Exception();
+        }
+    }
+}

88351d304e49c9e2b8ffac29940981bea6e5244f
Antheius-AT <53980774+Antheius-AT@users.noreply.github.com>
2022-05-10 00:00:31 +0200

    Continued implementation of login form using attribute reflection

---
+++ Attribute_Reflection_UseCase1_UserInteraction/AppState.cs
@@ -0,0 +1,14 @@
+﻿namespace Basic_Reflection_UseCase1_Display
+{
+    public class AppState
+    {
+        public bool Visible { get; set; }
+
+        public event EventHandler? ChangedEvent;
+    
+        public void RaiseEvent()
+        {
+            this.ChangedEvent?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}

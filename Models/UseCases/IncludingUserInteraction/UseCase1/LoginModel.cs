270281e1dba53a098388bd59824192c9b593861d
Gregor <gaf@devcircle.at>
2022-05-09 19:08:13 +0200

    Started implementation of attribute reflection use case 1 user input login model

--- Models/UseCases/IncludingUserInteraction/UseCase1/LoginModel.cs
+++ Models/UseCases/IncludingUserInteraction/UseCase1/LoginModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models.Metadata;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,13 @@ using System.Threading.Tasks;
 
 namespace Models.UseCases.IncludingUserInteraction.UseCase1
 {
+    [InputForm]
     public class LoginModel
     {
+        [Required]
         public string? UserName { get; set; }
 
+        [Required]
         public string? Password { get; set; }
 
         public string? PasswordConfirmation { get; set; }

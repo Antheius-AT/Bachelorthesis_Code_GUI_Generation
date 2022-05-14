0cacc13c3a85bba5d04245525d80e70fc0b43234
Antheius-AT <53980774+Antheius-AT@users.noreply.github.com>
2022-05-14 15:26:21 +0200

    Added source generator and shell for testing source generator. Non functional as of now

---
+++ SourceCodeGenerators/CUISpecificationGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Xml.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace SourceCodeGenerators
+{
+    [Generator]
+    public class CUISpecificationGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var sourceBuilder = new StringBuilder(@"
+using System;
+namespace HelloWorldTest
+{
+    public static class HelloWorld
+    {   
+        public static void Say()
+            {
+                Console.WriteLine(""Hello"");
+            }
+    }
+}");
+
+            context.AddSource("HelloWorldGenerator", SourceText.From(sourceBuilder.ToString()));
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}

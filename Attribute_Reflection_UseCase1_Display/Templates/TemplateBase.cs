a37ffdd68aba7f57524324cd9f1c14b67995621e
Gregor <gaf@devcircle.at>
2022-05-23 21:11:16 +0200

    Refactored template dynamic component use case dashboard attribute reflection

---
+++ Attribute_Reflection_UseCase1_Display/Templates/TemplateBase.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Attribute_Reflection_UseCase1_DisplayOnly_Templates.Templates
+{
+    public abstract class TemplateBase : ComponentBase
+    {
+    }
+}

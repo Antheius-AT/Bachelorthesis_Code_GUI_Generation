3ba609089b723ae307d55e6fd54ae18eb1df74b0
Gregor <gaf@devcircle.at>
2022-06-04 10:19:30 +0200

    Refactor basic reflection projects

---
+++ Basic_Reflection_UseCase2_Display/Helpers/TemplateResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Basic_Reflection_UseCase2_Display_Template.Helpers
+{
+    public class TemplateResult
+    {
+        public TemplateResult(Type templateType, object? value, string label = "")
+        {
+            TemplateType = templateType;
+            Value = value;
+            Label = label;
+        }
+
+        public Type TemplateType { get; }
+        public object? Value { get; }
+
+        public string Label { get; }
+    }
+}

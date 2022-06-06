0642e76a3eaa4d6dfac4316f3165aaa2c720d4c5
Antheius-AT <53980774+Antheius-AT@users.noreply.github.com>
2022-06-06 23:46:28 +0200

    Refactoring templates

--- Attribute_Reflection_UseCase1_Display/Templates/TemplateBase.cs
+++ Attribute_Reflection_UseCase1_Display/Templates/TemplateBase.cs
@@ -4,5 +4,7 @@ namespace Attribute_Reflection_UseCase1_DisplayOnly_Templates.Templates
 {
     public abstract class TemplateBase : ComponentBase
     {
+        [Parameter]
+        public string? Label { get; set; }
     }
 }
--- Attribute_Reflection_UseCase1_UserInteraction_Templates/Templates/TemplateBase.cs
+++ Attribute_Reflection_UseCase1_UserInteraction_Templates/Templates/TemplateBase.cs
@@ -1,19 +1,21 @@
-﻿using Attribute_Reflection_UseCase1_UserInteraction_Templates.Helpers;
+﻿using System.Reflection;
+using Attribute_Reflection_UseCase1_UserInteraction_Templates.Helpers;
 using Microsoft.AspNetCore.Components;
 
 namespace Attribute_Reflection_UseCase1_UserInteraction_Templates.Templates
 {
     public abstract class TemplateBase : ComponentBase
     {
-        /// <summary>
-        /// Event to signal that value changed. 
-        /// Could be a string, however I choose object for it to be a little more realistic (as not too many UIs only consist of string values)
-        /// </summary>
-        public event EventHandler<ValueChangedEventArgs> ValueChanged;
-        
-        protected virtual void RaiseValueChanged(object changed, string propertyName)
-        {
-            ValueChanged?.Invoke(this, new ValueChangedEventArgs(changed, propertyName));
-        }
+        [Parameter]
+        public string? Label { get; set; }
+
+        [Parameter]
+        public EventCallback<ValueChangedArguments> ValueChanged { get; set; }
+
+        [Parameter]
+        public PropertyInfo? PropertyInfo { get; set; }
+
+        [Parameter]
+        public object? AccessorInstance { get; set; }
     }
 }
